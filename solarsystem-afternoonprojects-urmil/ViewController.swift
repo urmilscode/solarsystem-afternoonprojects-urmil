a6beb0cfde79fda58e0d77f4400b092e7a118485
urmil <ucodes28@gmail.com>
2020-09-06 16:04:20 -0500

    Initial Commit

---
+++ solarsystem-afternoonprojects-urmil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  solarsystem-afternoonprojects-urmil
+//
+//  Created by Urmil Shah on 9/6/20.
+//  Copyright © 2020 Spark28 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
